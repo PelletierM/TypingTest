c2e76347869a8e878ae8211af6dbb1def8cff815
PelletierM <122843454+PelletierM@users.noreply.github.com>
2023-07-16 15:53:30 -0400

    Add authentication basis

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -9,9 +9,20 @@ def create_app():
     app = Flask(__name__)
     # Credentials to be updated on production environment
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://mysql:@localhost:3306/typingtest'
+    # IMPORTANT : Secret key must be updated on production environment
+    app.secret_key = b'randombytes'
     db.init_app(app)
 
     from .routes import routes
     app.register_blueprint(routes, url_prefix="/")
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
--- app_package/routes.py
+++ app_package/routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
-from . import db
 from sqlalchemy import text
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from .models import User
+from flask_login import login_user, logout_user, login_required
 
 routes = Blueprint("routes", __name__)
 
@@ -9,4 +11,18 @@ routes = Blueprint("routes", __name__)
 def index():
     #Test for database query
     result = db.session.execute(text('SELECT * FROM test')).all()
-    return render_template("index.html", template_check="Template working", result=result)
+    return render_template("base.html", template_check="Template working", result=result)
+
+@routes.route("/sign-up", methods=['GET', 'POST'])
+def signup():
+    #Get form data
+    #Check if email or username already exist
+    #Check if password meets requirements
+    #Check if passwords match
+
+
+    new_user = User(email=email, username=username, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return render_template("signup.html")
