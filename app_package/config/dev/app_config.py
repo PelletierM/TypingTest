b4bc497a735c0de790d156903870625b5aeb2d84
PelletierM <122843454+PelletierM@users.noreply.github.com>
2023-08-06 20:02:54 -0400

    Implement app config as independent file

---
+++ app_package/config/dev/app_config.py
@@ -0,0 +1,14 @@
+class Config(object):
+    SECRET_KEY = b"randombytes"
+    EMAIL_CONFIRM_SALT = b"randombytes"
+    SQLALCHEMY_DATABASE_URI = 'mariadb+mariadbconnector://mysql:@localhost:3306/typingtest'
+
+    # Flask Mail settings
+    MAIL_DEFAULT_SENDER = "example@app.com"
+    MAIL_SERVER = "smtp.app.com"
+    MAIL_PORT = 999
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_DEBUG = False
+    MAIL_USERNAME = "example@app.com"
+    MAIL_PASSWORD = "examplepassword"
